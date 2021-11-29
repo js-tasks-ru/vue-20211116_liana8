e4572d184fdca5343b6a2f9a7b2ce34e567daefe
Liana <Nurislamova_LF@mail.ru>
2021-11-29 22:38:04 +0500

    solution 03-MarkedList

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,60 +1,58 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
-// From https://jsonplaceholder.typicode.com/comments
 const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
-
-// Требуется создать Vue приложение
-
-import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
- 
-// Создайте Vue приложение
-const RootComponent = defineComponent({
-    name: 'Root',
-    data() {
-        return {
-            emails,
-            search: '',
-        };
-    },
+    'Eliseo@gardner.biz',
+    'Jayne_Kuhic@sydney.com',
+    'Nikita@garfield.biz',
+    'Lew@alysha.tv',
+    'Hayden@althea.biz',
+    'Presley.Mueller@myrl.com',
+    'Dallas@ole.me',
+    'Mallory_Kunze@marie.org',
+    'Meghan_Littel@rene.us',
+    'Carmen_Keeling@caroline.name',
+    'Veronica_Goodwin@timmothy.net',
+    'Oswald.Vandervort@leanne.org',
+    'Kariane@jadyn.tv',
+    'Nathan@solon.io',
+    'Maynard.Hodkiewicz@roberta.com',
+    'Christine@ayana.info',
+    'Preston_Hudson@blaise.tv',
+    'Vincenza_Klocko@albertha.name',
+    'Madelynn.Gorczany@darion.biz',
+    'Mariana_Orn@preston.org',
+    'Noemie@marques.me',
+    'Khalil@emile.co.uk',
+    'Sophia@arianna.co.uk',
+    'Jeffery@juwan.us',
+    'Isaias_Kuhic@jarrett.net',
+  ];
+   
+  // Требуется создать Vue приложение
+   
+  import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
+   
+  // Создайте Vue приложение
+  const RootComponent = defineComponent({
+      name: 'Root',
+      data() {
+          return {
+              emails,
+              search: '',
+          };
+      },
+      
+      computed: { 
+          filteredEmails() {      
+              return this.emails.map((email) => {
+                return { email: email, 
+                         marked: email.toLowerCase().includes(this.search.toLowerCase()) && this.search != ''}});
+          },     
+      },
+   
+  });
     
-    computed: { 
-        filteredEmails() {
-            return this.emails.map((email) => { return { email: email, marked: 'dg'}});
-            //const marked = (email) => emails.includes(this.search); 
-            //return emails;            
-        //    
-        //    return { email: this.email, marked: marked };  
-        },     
-    },
+  const app = createApp(RootComponent);
+   
+  const vm = app.mount('#app');
+   
 
-});
-  
-const app = createApp(RootComponent);
- 
-const vm = app.mount('#app');
+   
