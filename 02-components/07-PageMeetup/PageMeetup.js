54bb3834fec4b6fc9310a7e159d2859750e990f8
Liana <Nurislamova_LF@mail.ru>
2021-12-16 19:12:28 +0500

    solve components

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,26 +1,65 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
-
+import MeetupView from '../06-MeetupView/MeetupView.js';
+import { fetchMeetupById } from './meetupService.js';
+ 
 export default defineComponent({
   name: 'PageMeetup',
-
+ 
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,    
   },
-
+ 
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+ 
+  data() {
+    return {
+      meetup: null,
+      error: '',
+    };
+  },
+  
+  watch : {
+    meetupId:function(id) {
+       this.getMeetup(this.meetupId);
+    },     
+  },
+ 
+  created() {
+    this.getMeetup(this.meetupId);
+  },
+ 
+  methods: {
+    async getMeetup(id) {        
+        this.meetup = null;     
+        this.error = ''; 
+        fetchMeetupById(id).then(
+          (meetup) => (this.meetup = meetup),
+          (error) => (this.error = error.message),
+        );  
+    },
+  },  
+ 
   template: `
-    <div class="page-meetup">
-      <!-- meetup view -->
-
-      <ui-container>
+    <div class="page-meetup">  
+      <meetup-view v-if="meetup" :meetup="meetup"></meetup-view>
+     
+      <ui-container v-if="!meetup && error===''">
         <ui-alert>Загрузка...</ui-alert>
-      </ui-container>
-
-      <ui-container>
-        <ui-alert>error</ui-alert>
-      </ui-container>
+      </ui-container>   
+ 
+      <ui-container v-if="error!=''">
+        <ui-alert>{{ error }}</ui-alert>
+      </ui-container>    
+    
     </div>`,
 });
+ 
